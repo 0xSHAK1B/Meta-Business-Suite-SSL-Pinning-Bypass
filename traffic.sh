42f139ddc8e2f32ddf7a704892d96b4ce69eac81
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-03-02 04:33:09 +0600

    Add traffic.sh with HTTP request headers

---
+++ traffic.sh
@@ -0,0 +1,44 @@
+host: b-graph.facebook.com
+
+x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
+
+x-fb-connection-type: dummy
+
+app-scope-id-header: 8455694a-1efe-4076-8913-308312956679
+
+x-zero-state: unknown
+
+authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
+
+x-fb-sim-hni: 470007
+
+x-fb-net-hni: 470007
+
+content-type: application/x-www-form-urlencoded
+
+x-graphql-client-library: graphservice
+
+x-graphql-request-purpose: fetch
+
+x-tigon-is-retry: False
+
+x-zero-f-device-id: 9d4ff08a-4542-4d87-a2ad-fa78843de3a4
+
+x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
+
+user-agent: [FBAN/PAAA;FBAV/542.0.0.48.106;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBBV/891524846;FB_FW/2;FBSN/Android;FBDI/9d4ff08a-4542-4d87-a2ad-fa78843de3a4;FBCR/TelKila;FBMF/Google;FBBD/Google;FBDV/Pixel;FBSV/10;FBCA/arm64-v8a:null;]
+
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+
+content-encoding: gzip
+
+accept-encoding: gzip, deflate
+
+x-fb-http-engine: Tigon/Liger
+
+x-fb-client-ip: True
+
+x-fb-server-cluster: True
+
+x-fb-conn-uuid-client: 6f9a0bf2d5e33284030d79aaaec9a88d
+
