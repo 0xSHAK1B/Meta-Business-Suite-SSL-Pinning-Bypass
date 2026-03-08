c16f2c74178e2f582caf5fcee92f00e8579952d2
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-03-08 08:51:05 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,18 +1,20 @@
+POST /graphql HTTP/2
+
 host: b-graph.facebook.com
 
 x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
 
 x-fb-connection-type: dummy
 
-app-scope-id-header: 8455694a-1efe-4076-8913-308312956679
+app-scope-id-header: 365a8e98-5bee-4183-9bb9-de51038321a0
 
 x-zero-state: unknown
 
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
 
-x-fb-sim-hni: 470007
+x-fb-sim-hni: 470002
 
-x-fb-net-hni: 470007
+x-fb-net-hni: 470002
 
 content-type: application/x-www-form-urlencoded
 
@@ -22,11 +24,11 @@ x-graphql-request-purpose: fetch
 
 x-tigon-is-retry: False
 
-x-zero-f-device-id: 9d4ff08a-4542-4d87-a2ad-fa78843de3a4
+x-zero-f-device-id: 365a8e98-5bee-4183-9bb9-de51038321a0
 
 x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
 
-user-agent: [FBAN/PAAA;FBAV/542.0.0.48.106;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBBV/891524846;FB_FW/2;FBSN/Android;FBDI/9d4ff08a-4542-4d87-a2ad-fa78843de3a4;FBCR/TelKila;FBMF/Google;FBBD/Google;FBDV/Pixel;FBSV/10;FBCA/arm64-v8a:null;]
+user-agent: [FBAN/PAAA;FBAV/543.0.0.46.107;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBBV/896471206;FB_FW/2;FBSN/Android;FBDI/365a8e98-5bee-4183-9bb9-de51038321a0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBDV/Pixel;FBSV/10;FBCA/arm64-v8a:null;]
 
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
@@ -40,5 +42,4 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: 6f9a0bf2d5e33284030d79aaaec9a88d
-
+x-fb-conn-uuid-client: fdd91a4b40e3de0638d848d14494b7b2
