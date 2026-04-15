f0545fd982339a048b7b730030132c3e518ef056
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-04-15 13:18:48 +0600

    Update HTTP headers in traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -2,13 +2,7 @@ POST /graphql HTTP/2
 
 host: b-graph.facebook.com
 
-x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
-
-x-fb-connection-type: dummy
-
-app-scope-id-header: e0ef5d89-c7bc-4863-9c06-54715c8617da
-
-x-zero-state: unknown
+x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
 
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
 
@@ -16,21 +10,25 @@ x-fb-sim-hni: 47007
 
 x-fb-net-hni: 47002
 
+user-agent: [FBAN/PAAA;FBAV/548.0.0.74.107;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/936591379;FB_FW/2;FBSN/Android;FBDI/null;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
+
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+
 content-type: application/x-www-form-urlencoded
 
 x-graphql-client-library: graphservice
 
-x-graphql-request-purpose: fetch
+x-fb-friendly-name: AutofillUpdateNegativeInteractionCountMutation
+
+x-zero-state: unknown
+
+x-fb-connection-type: dummy
+
+app-scope-id-header: d8cf0646-c7b8-4c13-8da0-e209c858803f
+
+x-fb-navigation-chain-status: null_user_session
 
 x-tigon-is-retry: False
-
-x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
-
-user-agent: [FBAN/PAAA;FBAV/545.0.0.58.109;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/909563321;FB_FW/2;FBSN/Android;FBDI/null;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
-
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-
-content-encoding: gzip
 
 accept-encoding: gzip, deflate
 
@@ -40,4 +38,4 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: Zg+0gb/PXj92BvEokiUZqw==
+x-fb-conn-uuid-client: i0iLldvkDpUil7Xk5gqTEQ==
