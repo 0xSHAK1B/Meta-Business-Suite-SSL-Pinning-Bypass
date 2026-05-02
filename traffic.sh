6787d1bf2059f3d542c9588c7edd047ae765e030
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-05-02 13:05:00 +0600

    Update user-agent and x-fb headers in traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -4,13 +4,17 @@ host: b-graph.facebook.com
 
 x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
 
+x-zero-state: unknown
+
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
+
+x-fb-connection-type: dummy
+
+app-scope-id-header: ec275832-bc3a-4d9c-98aa-3017524ce92f
 
 x-fb-sim-hni: 47007
 
 x-fb-net-hni: 47002
-
-user-agent: [FBAN/PAAA;FBAV/548.0.0.74.107;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/936591379;FB_FW/2;FBSN/Android;FBDI/null;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
 
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
@@ -18,15 +22,9 @@ content-type: application/x-www-form-urlencoded
 
 x-graphql-client-library: graphservice
 
-x-fb-friendly-name: AutofillUpdateNegativeInteractionCountMutation
+user-agent: [FBAN/PAAA;FBAV/551.0.0.55.106;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/955656481;FB_FW/2;FBSN/Android;FBDI/null;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
 
-x-zero-state: unknown
-
-x-fb-connection-type: dummy
-
-app-scope-id-header: d8cf0646-c7b8-4c13-8da0-e209c858803f
-
-x-fb-navigation-chain-status: null_user_session
+x-fb-friendly-name: AutofillFetchContactDataMultipleEntriesQuery
 
 x-tigon-is-retry: False
 
@@ -38,4 +36,5 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: i0iLldvkDpUil7Xk5gqTEQ==
+x-fb-conn-uuid-client: p7jcBP+rhU6txOG77OyXBQ==
+
