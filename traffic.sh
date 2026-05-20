c27c967fb7c60819189571e2196944fb1811a130
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-05-20 21:30:34 +0600

    Modify HTTP headers in traffic.sh for GraphQL
    Updated HTTP headers for GraphQL requests in traffic.sh, including changes to request analytics tags, connection type, and user agent.

--- traffic.sh
+++ traffic.sh
@@ -2,33 +2,37 @@ POST /graphql HTTP/2
 
 host: b-graph.facebook.com
 
-x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
+x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
+
+x-fb-connection-type: dummy
+
+app-scope-id-header: d45c0677-bcef-4faf-96e0-4520223945fd
 
 x-zero-state: unknown
 
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
 
-x-fb-connection-type: dummy
-
-app-scope-id-header: 8e5f3491-da6a-4e6e-8428-c4ad0662b423
-
 x-fb-sim-hni: 47007
 
 x-fb-net-hni: 47002
-
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
 content-type: application/x-www-form-urlencoded
 
 x-graphql-client-library: graphservice
 
-user-agent: [FBAN/PAAA;FBAV/552.0.0.59.107;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/962140016;FB_FW/2;FBSN/Android;FBDI/8e5f3491-da6a-4e6e-8428-c4ad0662b423;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
-
-x-zero-f-device-id: 8e5f3491-da6a-4e6e-8428-c4ad0662b423
-
-x-fb-friendly-name: AutofillFetchSettingsQuery
+x-graphql-request-purpose: fetch
 
 x-tigon-is-retry: False
+
+x-zero-f-device-id: d45c0677-bcef-4faf-96e0-4520223945fd
+
+x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
+
+user-agent: [FBAN/PAAA;FBAV/553.0.0.55.109;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBBV/968828665;FB_FW/2;FBSN/Android;FBDI/d45c0677-bcef-4faf-96e0-4520223945fd;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBDV/Z35;FBSV/11;FBCA/arm64-v8a:null;]
+
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+
+content-encoding: gzip
 
 accept-encoding: gzip, deflate
 
@@ -38,4 +42,4 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: A6oUDcH1f+bwavduQJzUqg==
+x-fb-conn-uuid-client: zQYNAy9R6BrIDYMX0nIhpg==
