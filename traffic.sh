89e26025548ed1df016259f89fd1c667e6646e07
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-19 18:10:05 +0600

    Update headers in traffic.sh for API request

--- traffic.sh
+++ traffic.sh
@@ -2,7 +2,7 @@ POST /graphql HTTP/2
 host: b-graph.facebook.com
 x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
 x-fb-connection-type: dummy
-app-scope-id-header: 6e56f6ac-2f87-4480-8c28-b074ed9b33be
+app-scope-id-header: aece2459-321c-4d5f-ba4b-bfa315d64cbe
 x-zero-state: unknown
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
 content-encoding: gzip
@@ -11,11 +11,11 @@ content-type: application/x-www-form-urlencoded
 x-graphql-client-library: graphservice
 x-graphql-request-purpose: fetch
 x-tigon-is-retry: False
-x-zero-f-device-id: c6c44de3-5dab-416f-98f4-25b534b14f83
+x-zero-f-device-id: d193e217-62a2-42aa-8a82-b8afb460b4bf
 x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
-user-agent: [FBAN/PAAA;FBAV/557.0.0.48.108;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/991473983;FB_FW/2;FBSN/Android;FBDI/c6c44de3-5dab-416f-98f4-25b534b14f83;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
+user-agent: [FBAN/PAAA;FBAV/558.0.0.48.106;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/996251683;FB_FW/2;FBSN/Android;FBDI/d193e217-62a2-42aa-8a82-b8afb460b4bf;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
 accept-encoding: gzip, deflate
 x-fb-http-engine: Tigon/Liger
 x-fb-client-ip: True
 x-fb-server-cluster: True
-x-fb-conn-uuid-client: ctlu/8XpiefjXthG8dTn7A==
+x-fb-conn-uuid-client: 7JGH0nMGhp2tSPE9IS/tSw==
