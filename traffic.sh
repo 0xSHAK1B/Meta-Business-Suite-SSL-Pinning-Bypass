4f02f73dd0c7d528c410ba23eb1d168dd1e96f5b
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-27 10:30:21 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,21 +1,20 @@
 POST /graphql HTTP/2
 host: b-graph.facebook.com
-x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
-x-fb-connection-type: dummy
-app-scope-id-header: aece2459-321c-4d5f-ba4b-bfa315d64cbe
+x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
+x-fb-friendly-name: AutofillFetchContactDataMultipleEntriesQuery
+x-zero-f-device-id: 868461dc-21b3-4965-8645-7ffe481ac7ee
 x-zero-state: unknown
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
-content-encoding: gzip
+x-fb-connection-type: dummy
+app-scope-id-header: 874e6ec6-3f29-46d5-a3f7-fb42f48d037a
+x-fb-sim-hni: 47002
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 content-type: application/x-www-form-urlencoded
 x-graphql-client-library: graphservice
-x-graphql-request-purpose: fetch
+user-agent: [FBAN/PAAA;FBAV/559.0.0.41.105;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/1001374415;FB_FW/2;FBSN/Android;FBDI/868461dc-21b3-4965-8645-7ffe481ac7ee;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
 x-tigon-is-retry: False
-x-zero-f-device-id: d193e217-62a2-42aa-8a82-b8afb460b4bf
-x-fb-friendly-name: com.bloks.www.bloks.caa.login.async.send_login_request
-user-agent: [FBAN/PAAA;FBAV/558.0.0.48.106;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/996251683;FB_FW/2;FBSN/Android;FBDI/d193e217-62a2-42aa-8a82-b8afb460b4bf;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
 accept-encoding: gzip, deflate
 x-fb-http-engine: Tigon/Liger
 x-fb-client-ip: True
 x-fb-server-cluster: True
-x-fb-conn-uuid-client: 7JGH0nMGhp2tSPE9IS/tSw==
+x-fb-conn-uuid-client: nWDR06OIQ1sTKNypK/Bxrg==
