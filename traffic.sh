17e3adc1265164f6244ba3be685a07d1a366cdc1
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-07-20 23:34:38 +0600

    Update HTTP headers in traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,20 +1,23 @@
 POST /graphql HTTP/2
 host: b-graph.facebook.com
 x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
-x-fb-friendly-name: AutofillFetchContactDataMultipleEntriesQuery
-x-zero-f-device-id: 868461dc-21b3-4965-8645-7ffe481ac7ee
-x-zero-state: unknown
 authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
-x-fb-connection-type: dummy
-app-scope-id-header: 874e6ec6-3f29-46d5-a3f7-fb42f48d037a
 x-fb-sim-hni: 47002
+x-fb-net-hni: 47002
+user-agent: [FBAN/PAAA;FBAV/562.0.0.24.107;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/1017100200;FB_FW/2;FBSN/Android;FBDI/788d04cf-0522-4a85-86c4-40be6fdf99be;FBCR/Robi;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 content-type: application/x-www-form-urlencoded
 x-graphql-client-library: graphservice
-user-agent: [FBAN/PAAA;FBAV/559.0.0.41.105;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/1001374415;FB_FW/2;FBSN/Android;FBDI/868461dc-21b3-4965-8645-7ffe481ac7ee;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
+x-zero-f-device-id: 788d04cf-0522-4a85-86c4-40be6fdf99be
+x-fb-friendly-name: AutofillUpdateNegativeInteractionCountMutation
+x-zero-state: unknown
+x-fb-connection-type: dummy
+app-scope-id-header: 788d04cf-0522-4a85-86c4-40be6fdf99be
+x-fb-navigation-chain-status: null_user_session
 x-tigon-is-retry: False
 accept-encoding: gzip, deflate
 x-fb-http-engine: Tigon/Liger
 x-fb-client-ip: True
 x-fb-server-cluster: True
-x-fb-conn-uuid-client: nWDR06OIQ1sTKNypK/Bxrg==
+x-fb-conn-uuid-client: Q1eqYy7cvl1ds52i9sqIxQ==
+
