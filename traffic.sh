d8b1b1db8560ea54ebd89d60d87e6bd39d2642d7
MUHAMMAD SHAKIB <shakibb1928u@gmail.com>
2026-07-31 21:15:53 +0600

    Replace HTTP request with curl command

--- traffic.sh
+++ traffic.sh
@@ -1,23 +1,22 @@
-POST /graphql HTTP/2
-host: b-graph.facebook.com
-x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
-authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc
-x-fb-sim-hni: 47002
-x-fb-net-hni: 47002
-user-agent: [FBAN/PAAA;FBAV/562.0.0.24.107;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/1017100200;FB_FW/2;FBSN/Android;FBDI/788d04cf-0522-4a85-86c4-40be6fdf99be;FBCR/Robi;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-content-type: application/x-www-form-urlencoded
-x-graphql-client-library: graphservice
-x-zero-f-device-id: 788d04cf-0522-4a85-86c4-40be6fdf99be
-x-fb-friendly-name: AutofillUpdateNegativeInteractionCountMutation
-x-zero-state: unknown
-x-fb-connection-type: dummy
-app-scope-id-header: 788d04cf-0522-4a85-86c4-40be6fdf99be
-x-fb-navigation-chain-status: null_user_session
-x-tigon-is-retry: False
-accept-encoding: gzip, deflate
-x-fb-http-engine: Tigon/Liger
-x-fb-client-ip: True
-x-fb-server-cluster: True
-x-fb-conn-uuid-client: Q1eqYy7cvl1ds52i9sqIxQ==
-
+curl --http2 -L -X POST 'https://b-graph.facebook.com/graphql' \
+-H 'User-Agent: [FBAN/PAAA;FBAV/563.0.0.50.106;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_US;FBBV/1023339812;FB_FW/2;FBSN/Android;FBDI/da954aa8-4669-4d33-ad55-f73894e245e8;FBCR/Robi;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;]' \
+-H 'Accept-Encoding: gzip, deflate' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-H 'x-fb-request-analytics-tags: {"network_tags":{"product":"121876164619130","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}' \
+-H 'x-fb-connection-type: dummy' \
+-H 'app-scope-id-header: 788d04cf-0522-4a85-86c4-40be6fdf99be' \
+-H 'x-zero-state: unknown' \
+-H 'authorization: OAuth 121876164619130|1ab2c5c902faedd339c14b2d58e929dc' \
+-H 'x-fb-sim-hni: 47002' \
+-H 'x-fb-net-hni: 47002' \
+-H 'x-graphql-client-library: graphservice' \
+-H 'x-graphql-request-purpose: fetch' \
+-H 'x-tigon-is-retry: False' \
+-H 'x-zero-f-device-id: da954aa8-4669-4d33-ad55-f73894e245e8' \
+-H 'x-fb-friendly-name: FbBloksAsyncComponentsQuery-com.bloks.www.ccs.conversational_support.fb_forgot_password_button.component.query' \
+-H 'x-zero-eh: 664c0faaac849cb891d0a261fbb72a12' \
+-H 'content-encoding: gzip' \
+-H 'x-fb-http-engine: Tigon/Liger' \
+-H 'x-fb-client-ip: True' \
+-H 'x-fb-server-cluster: True' \
+-H 'x-fb-conn-uuid-client: BGiuiWyuXOveSyoXNbS7Pg==' \
